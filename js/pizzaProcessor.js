6a95c293361ebbebd094efcf62037b364095e99a
prufrock123 <shawnmllr86@gmail.com>
2014-12-17 17:13:14 -0600

    Initial commit and sets up some dependencies and backbone structure.

---
+++ js/pizzaProcessor.js
@@ -0,0 +1,9 @@
+;(function(window, undefined){
+
+var pizzas = app.pizzas
+
+
+
+
+
+})(window, undefined);
---
+++ js/views/pizzaCounterView.js
@@ -0,0 +1,9 @@
+;(function(window, undefined){
+
+	window.app = window.app || {};
+
+	var PizzaCounterView = Backbone.View.extend({
+		
+	})
+
+})(window, undefined);
